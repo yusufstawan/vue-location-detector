edac1c6f661e6217e9505523eb43a5c43bb03140
yusufstawan <yusufsetiyawan45@gmail.com>
2023-01-13 23:33:03 +0700

    first commit

---
+++ src/pages/UserLocation.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="ui two column centered grid">
+    <div class="column">
+      <form class="ui segment large form">
+        <div class="ui message red"></div>
+        <div class="ui segment">
+          <div class="field">
+            <div class="ui right icon input large">
+              <input type="text" placeholder="Enter your address" />
+              <i class="dot circle link icon" @click="locatorButtonPressed"></i>
+            </div>
+          </div>
+          <button class="ui button">Go</button>
+        </div>
+      </form>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  methods: {
+    locatorButtonPressed() {
+      if (navigator.geolocation) {
+      } else {
+        console.log("Your browser does not support geolocation API");
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.ui.button,
+.dot.circle.icon {
+  background-color: #ff5a5f;
+  color: white;
+}
+</style>
