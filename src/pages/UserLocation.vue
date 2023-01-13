8af885aea50b101232fe3a71aeab65d28d2cc0e6
yusufstawan <yusufsetiyawan45@gmail.com>
2023-01-14 00:15:57 +0700

    add axios

--- src/pages/UserLocation.vue
+++ src/pages/UserLocation.vue
@@ -6,7 +6,11 @@
         <div class="ui segment">
           <div class="field">
             <div class="ui right icon input large">
-              <input type="text" placeholder="Enter your address" />
+              <input
+                type="text"
+                placeholder="Enter your address"
+                v-model="address"
+              />
               <i class="dot circle link icon" @click="locatorButtonPressed"></i>
             </div>
           </div>
@@ -18,13 +22,53 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
+  data() {
+    return {
+      address: ""
+    };
+  },
+
   methods: {
     locatorButtonPressed() {
       if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          position => {
+            this.getAddressFrom(
+              position.coords.latitude,
+              position.coords.longitude
+            );
+          },
+          error => {
+            console.log(error.message);
+          }
+        );
       } else {
         console.log("Your browser does not support geolocation API");
       }
+    },
+    getAddressFrom(lat, long) {
+      axios
+        .get(
+          "https://maps.googleapis.com/maps/api/geocode/json?latlng=" +
+            lat +
+            "," +
+            long +
+            "&key=AIzaSyA6TcIPRePIJU2BYVlTOCSYmobIBL9_6VI"
+        )
+        .then(response => {
+          if (response.data.error_message) {
+            console.log(response.data.error_message);
+          } else {
+            this.address = response.data.results[0].formattes_address;
+            // console.log(response.data.results[0].formattes_address);
+          }
+        })
+        .catch(error => {
+          console.log(error.message);
+        });
     }
   }
 };
